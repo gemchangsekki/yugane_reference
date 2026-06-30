c657628c4f85c83165d32fc1bafce3faea2a60de
speeddemon18 <5p33dd3mon18@gmail.com>
2026-06-30 17:19:24 +0900

    navigation scroll 기능 추가(추후 transition 추가 가능)

--- js/common.js
+++ js/common.js
@@ -1,5 +1,17 @@
+const navigation = document.querySelector(".navigation");
+
 const heroSec = document.getElementById("hero-sec");
 const heroSecBtn = document.querySelectorAll("#hero-sec-btn");
+
+window.addEventListener("scroll", () => {
+  const scrollY = window.scrollY;
+
+  if(scrollY > 50) {
+    navigation.style.display = "inline";
+  } else {
+    navigation.style.display = "none";
+  }
+})
 
 heroSecBtn.forEach((button, index) => {
   button.addEventListener("click", () => {
