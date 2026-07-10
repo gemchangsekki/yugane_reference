90ea8f9a0e3b11e5412e4d6e3b6a1e08b77d5dff
ron98 <rlgns1818@gmail.com>
2026-07-10 17:27:28 +0900

    inquiry-policy-icon 체크 시 이미지경로 변경 작업 시작

--- js/index.js
+++ js/index.js
@@ -117,6 +117,13 @@ let autoHyphen = (userPhoneNumber) => {
   }
 };
 
+const policyCheckbox = document.querySelector(".inquiry-policy-input");
+const policyCheck = document.querySelector(".inquiry-policy-check");
+
+policyCheck.addEventListener("click", (e) => {
+  
+});
+
 let userPhoneNumber = document.querySelector('#inquiry-input-tel');
 
 userPhoneNumber.oninput = (e) => {
